cbc14f5e522f4276ae69549c130814188d0a9e7c
Jan Magne Andersen <jan@pallados.no>
2018-12-31 10:23:15 +0000

    Added script for setting up environment and downloading training data

---
+++ setup.sh
@@ -0,0 +1,12 @@
+source activate pytorch_p36
+
+conda install -c menpo opencv
+
+wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip
+
+wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/lfw.zip
+
+unzip dogImages.zip 
+
+unzip lfw.zip
+
